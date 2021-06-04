76e9abe441965cbd391423272f0fd75dcd90c219
peta <coisasdohapo@gmail.com>
2021-06-04 16:04:44 +0100

    Making pop up Messages for the door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -15,23 +15,28 @@ public class Door : MonoBehaviour
     
     bool isOpen = false;
 
+    public GameObject popUp;
+
+
    void Start()
    {
        Player = GameObject.Find("Player");
        CheckParts = GameObject.Find("PartsManager").GetComponent<coinManager>();
-       
+       popUp.SetActive(false);
 
    }
     void OnTriggerEnter2D(Collider2D col)
     {
-       if(col.gameObject.name.Equals("PrefabAcido1"))
+       
+       if(col.gameObject.name == "Acid")
         {
             Debug.Log("I GOT ACID ON ME EWWWWWW");
             isOpen = true;
            
         }
         if(col.gameObject.name.Equals("Player"))
-        {
+        {   
+            popUp.SetActive(true);
             Debug.Log("I see you/n YOU NEED MORE PARTS");
             isOpen = true;
            
@@ -41,7 +46,13 @@ public class Door : MonoBehaviour
             
             GetComponent<BoxCollider2D>().enabled = false;
         }
+        
+            
 
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        popUp.SetActive(false);
     }
    
 }
