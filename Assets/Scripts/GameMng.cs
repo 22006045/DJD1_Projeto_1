4dd660f63a13937569b172cfcc50226f809c2c29
peta <coisasdohapo@gmail.com>
2021-06-10 11:31:44 +0100

    Fixing Acid adding pause to game

--- Assets/Scripts/GameMng.cs
+++ Assets/Scripts/GameMng.cs
@@ -5,17 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class GameMng : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenuUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Loading");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quiting");
+        Application.Quit();
+    }
+    
 
     public void BackToMainMenu(float time)
     {
