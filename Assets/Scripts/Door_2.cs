479fc483ae8b3b94cb33c541d95ca3aa11f29cae
peta <coisasdohapo@gmail.com>
2021-06-13 16:28:34 +0100

    Level Build
    New script ("Door_2") for the second stage of the level
    
    Jump animation ajustments
    
    Placed the definitive blocks
    
    Stage 1 & 2 added
    
    More enemies and traps added

---
+++ Assets/Scripts/Door_2.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_2 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    private coinManager CheckParts;
+
+    private GameObject Player;
+
+    public BoxCollider2D collider;
+    
+    bool isOpen = false;
+
+    public GameObject popUp;
+
+    private Animator led_green;
+
+    private Animator open_door;
+
+    public AudioSource DoorSound;
+
+
+   void Start()
+   {
+       Player = GameObject.Find("Player");
+       CheckParts = GameObject.Find("PartsManager").GetComponent<coinManager>();
+       popUp.SetActive(false);
+       led_green = GetComponent<Animator>();
+       open_door = GetComponent<Animator>();
+       DoorSound = GetComponent<AudioSource>();
+
+   }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       
+       if(col.gameObject.tag == "Acid")
+        {
+            Debug.Log("I GOT ACID ON ME EWWWWWW");
+            isOpen = true;
+            
+           
+        }
+        if(col.gameObject.name.Equals("Player"))
+        {   
+            popUp.SetActive(true);
+            Debug.Log("I see you/n YOU NEED MORE PARTS");
+            isOpen = true;
+           
+        }
+        if(isOpen == true && CheckParts.parts >=6)
+        {
+            open_door.SetBool("open",true);
+            led_green.SetBool("set_green",true);
+            GetComponent<BoxCollider2D>().enabled = false;
+            DoorSound.Play();
+        
+        }
+        else led_green.SetBool("set_green",false);
+       
+        if(isOpen == true && col.gameObject.tag == "Acid" )
+        {
+
+            GetComponent<BoxCollider2D>().enabled = false;
+
+        }
+        
+            
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        popUp.SetActive(false);
+    }
+   
+}
+
